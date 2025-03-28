9daf7dce8448e6e2c93c103c4b13a0baa78f32db
Raphael Fortunato <fortunatoraphael@hotmail.com>
2025-03-28 10:50:56 -0300

    Commit ajuste

--- demo/src/main/java/com/example/demo/repository/PessoaRepository.java
+++ demo/src/main/java/com/example/demo/repository/PessoaRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-
 	
 }
